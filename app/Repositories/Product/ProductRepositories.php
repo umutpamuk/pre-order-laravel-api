04ee5340232890b48198354b59245a227ac2f0d0
umutpamuk <umutpamuk59@gmail.com>
2023-05-28 21:38:01 +0300

    ProductRepositories.php stockDecrement ve findOrFail methodları eklendi.

--- app/Repositories/Product/ProductRepositories.php
+++ app/Repositories/Product/ProductRepositories.php
@@ -25,4 +25,35 @@ class ProductRepositories implements ProductRepositoriesInterface
         return Product::where('stock', '>', 0)->with('category')->get();
 
     }
+
+    /**
+     * @param int $id
+     * @return Product
+     */
+    public function findOrFail(int $id): Product
+    {
+        return Product::findOrFail($id);
+    }
+
+    /**
+     * @param int $productId
+     * @param int $quantity
+     * @return bool
+     */
+    public function stockDecrement(int $productId, int $quantity) : bool
+    {
+        $product = $this->findOrFail($productId);
+
+        if ($product->stock >= $quantity) {
+
+            $product->decrement('stock', $quantity);
+
+            return true;
+
+        } else {
+
+          return false;
+
+        }
+    }
 }
--- app/Repositories/Product/ProductRepositoriesInterface.php
+++ app/Repositories/Product/ProductRepositoriesInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Product;
 
+use App\Models\Product;
 use Illuminate\Support\Collection;
 
 interface ProductRepositoriesInterface
@@ -16,5 +17,18 @@ interface ProductRepositoriesInterface
      * @return Collection
      */
     public function allAvailable() : Collection;
+
+    /**
+     * @param int $id
+     * @return Product
+     */
+    public function findOrFail(int $id) : Product;
+
+    /**
+     * @param int $productId
+     * @param int $quantity
+     * @return bool
+     */
+    public function stockDecrement(int $productId, int $quantity) : bool;
 
 }
