e636cefdb776c2c653813633c9b68eba087f137f
umutpamuk <umutpamuk59@gmail.com>
2023-05-29 14:09:43 +0300

    User için seeder güncellendi.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class UserSeeder extends Seeder
 {
@@ -16,15 +17,25 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => "Umut",
-            'email' => "Pamuk",
+            'name' => "Umut Pamuk",
+            'email' => 'umutpamuk59@gmail.com',
             'password' => Hash::make("umutpamuk"),
             'role' => 'admin',
             ]);
 
+        $user = User::first();
+
+        PersonalAccessToken::create([
+            'tokenable_type' => get_class($user),
+            'tokenable_id' => $user->id,
+            'name' => 'API TOKEN',
+            'token' => '6fff2fdd38b31a4fbf02bfb27732523cec165daa082c7d46610bfa2a19b66f65',
+            'abilities' => ["*"],
+        ]);
+
         User::create([
-            'name' => "Ömer",
-            'email' => "Şahinler",
+            'name' => "Ömer Şahinler",
+            'email' => 'omersahinler@gmail.com',
             'password' => Hash::make("omersahinler"),
             'role' => 'user',
             ]);
